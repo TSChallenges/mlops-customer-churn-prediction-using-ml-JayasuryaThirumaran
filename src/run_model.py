b810fd0a00cbd3785632d0f75c226421078c73e9
JayasuryaThirumaran <jayasurya.t@talentsprint.com>
2024-10-14 12:52:54 +0000

    chan

--- src/run_model.py
+++ src/run_model.py
@@ -2,7 +2,7 @@ import os
 import pickle
 
 def load_model():
-    model_path = '/workspaces/mlops-churn-prediction/models/churn_model.pkl' 
+    model_path = '/workspaces/mlops-customer-churn-prediction-using-ml-JayasuryaThirumaran/models/churn_model.pkl' 
     with open(model_path, 'rb') as model_file:
         model = pickle.load(model_file)
     return model
